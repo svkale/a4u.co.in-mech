e961e52ff422d87a3ef10513aa4bb575fa27f290
Siddharth Kale <69069085+svkale@users.noreply.github.com>
2024-08-21 16:44:52 +0530

    Update google.js

--- website-modules/utils/putins/google.js
+++ website-modules/utils/putins/google.js
@@ -171,9 +171,20 @@ function putins_make_page_from_gdoc(request_obj, params) {
 	  */
 	      console.log("pasteHTML.");
 	  //"https://script.google.com/macros/s/AKfycbx7kmBiPvF2HwSHvUKJArTMw510MuuuiToUiCOMk5yE6G881pa-6VDUkfBiXmwj33IL/exec?fn=GetCell&id=1lmMF-pV1qrh6qbHx3_I-iZdkYILNvEDQC7CjJTa01tw&ssn=RecentNotices&cell=D1"
-	  fetch (j[2])
-		.then(x => x.text())
-		.then(y => nav_HTML += '<span class="u1" style="text-align: center;font-weight: bold;">' + y + "</span>");
+	  async function (){
+		let data = await fetch(j[2])
+  			.then(x => x.text())
+			.then(y => nav_HTML += '<span class="u1" style="text-align: center;font-weight: bold;">' + y + "</span>");
+  			.catch(error => { console.error(error); });
+    
+		console.log(data);
+		document.getElementById("demo").innerHTML += "<br><br>"+data
+	  }();
+	      
+	      
+	      //fetch (j[2])
+		//.then(x => x.text())
+		//.then(y => nav_HTML += '<span class="u1" style="text-align: center;font-weight: bold;">' + y + "</span>");
           var dyn_data_yn = false;
           //nav_HTML +=
           //  '<span class="u1" style="text-align: center;font-weight: bold;">' +
