c37af2e9a22f828e2ec34bfac770f2332cca61f3
Siddharth Kale <69069085+svkale@users.noreply.github.com>
2025-07-08 14:04:32 -0400

    Update google.js

--- website-modules/utils/putins/google.js
+++ website-modules/utils/putins/google.js
@@ -262,27 +262,27 @@ function putins_make_page_from_gdoc(request_obj, params) {
        } else if(j[1]=="indexSlideShow"){  //slideshow in index column
 	  //console.log("indexSlideShow");console.log(j);
 	  let url="https://script.google.com/macros/s/AKfycbzy53ifIUTm2YNc_T_uv1Y0RV0PaLlE8i00V2DTvzBFCuG1Q8ocrvguw4mKUfkiykJSHA/exec?fn=fileList&transpose=false&folderID="+j[2].split("/")[0],
-	      slideShowId="slideshow",timeInterval=3000,atag=true,labelOnImage=false;
+	      slideShowId="slideshow",timeInterval=3000,atag=true,labelOnImage=false,widthHeight="width:150px;height:200px;";
 	  if(j[2].split("/").length>=1 && j[2].split("/")[1]!=""){slideShowId=j[2].split("/")[1];}
 	  if(j[2].split("/").length>=2 && j[2].split("/")[2]!=""){timeInterval=j[2].split("/")[2];}
 	  if(j[2].split("/").length>=3 && j[2].split("/")[3]!=""){atag=j[2].split("/")[3];}
 	  if(j[2].split("/").length>=4 && j[2].split("/")[4]!=""){labelOnImage=j[2].split("/")[4];}
 	  //console.log(slideShowId,timeInterval,atag,labelOnImage);
-	  let div_str='<div align="center"><div id="'+slideShowId+'" style="max-width:500px;overflow:hidden;"></div></div>';
+	  let div_str='<div align="center"><div id="'+slideShowId+'" style="max-width:500px;overflow:hidden;'+widthHeight+'"></div></div>';
 	  nav_HTML +=
           '<section class="cont" style="--cont-width: 50%; margin-left: 25%;"><div align="center"><div id="'+slideShowId+'" style="max-width:150px;overflow:hidden;"></div></div></section>';
 	  getSlidesData(url,slideShowId,timeInterval,atag,labelOnImage);
        } else if(j[1]=="gSlideShow"){
 	  //console.log("gSlideShow");console.log(j);
 	  let url="https://script.google.com/macros/s/AKfycbzy53ifIUTm2YNc_T_uv1Y0RV0PaLlE8i00V2DTvzBFCuG1Q8ocrvguw4mKUfkiykJSHA/exec?fn=fileList&transpose=false&folderID="+j[2].split("/")[0],
-	      slideShowId="slideshow",timeInterval=3000,atag=false,labelOnImage=false;
+	      slideShowId="slideshow",timeInterval=3000,atag=false,labelOnImage=false,widthHeight="width:150px;height:200px;";
 	  if(j[2].split("/").length>=1 && j[2].split("/")[1]!=""){slideShowId=j[2].split("/")[1];}
 	  if(j[2].split("/").length>=2 && j[2].split("/")[2]!=""){timeInterval=j[2].split("/")[2];}
 	  if(j[2].split("/").length>=3 && j[2].split("/")[3]!=""){atag=j[2].split("/")[3];}
 	  if(j[2].split("/").length>=4 && j[2].split("/")[4]!=""){labelOnImage=j[2].split("/")[4];}
 	  //console.log(slideShowId,timeInterval,atag,labelOnImage);
 	
-	  let div_str='<div align="center"><div id="'+slideShowId+'" style="max-width:500px;overflow:hidden;"></div></div>';
+	  let div_str='<div align="center"><div id="'+slideShowId+'" style="max-width:500px;overflow:hidden;'+widthHeight+'"></div></div>';
 	  nav_HTML +=
 	  "<li id='EID_"+eidno+++"' class='u1 doc_page' onclick='location.hash=this.innerText.trim();document.getElementById(\"" +
           doc_ele_id +
@@ -899,7 +899,7 @@ function processSlideShow(text,slideShowId,timeInterval,atag,labelOnImage){
   let Data=JSON.parse(text);
   Data.sort();
   //console.log(Data);
-  let sshtml ='<div style="width:150px; overflow:hidden; position:relative; margin:auto; padding:1px;">';
+  let sshtml ='<div style="overflow:hidden; position:relative; margin:auto; padding:1px;">';
 
   sshtml +='<div class="slidesContainer" id="'+slideShowId+'_slidesContainer" style="display:flex; width:100%; height:100%; transition:transform 0.5s ease-in-out;">';
 
