f3b86ee92386f99a4d9c6f516632cbedff1a5404
Siddharth Kale <69069085+svkale@users.noreply.github.com>
2025-07-08 18:41:18 -0400

    Update google.js

--- website-modules/utils/putins/google.js
+++ website-modules/utils/putins/google.js
@@ -648,7 +648,7 @@ function putins_make_subpage_from_HTML(dom, doc_ele, element) {
       console.log(
         p,
         p.innerText,
-        p.innerHTML=p.innerText.substr(0, p.innerText.length - 7).substr(6);))
+        p.innerHTML=p.innerText.substr(0, p.innerText.length - 7).substr(6)
       )	
     );
   while (
